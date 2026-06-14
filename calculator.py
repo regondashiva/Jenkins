b166ce3e628568c317400813a09cfa7104b3ec6f
Regonda shiva <regondashiva2414@gmail.com>
2026-06-14 07:38:55 +0530

    Add basic calculator functionality in calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# calculator.py
+
+a = 20
+b = 10
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("Calculator Results")
+print("------------------")
+print("Addition =", add)
+print("Subtraction =", sub)
+print("Multiplication =", mul)
+print("Division =", div)
